e7a96d2048733acd2ce0b0ad955b6d5c86f8658c
ShaneN <shanen@silversalmon.com>
2015-02-26 13:24:18 -0700

    removed reactiveui list to simplify sample

--- TheApp/TheApp.Droid/Resources/Resource.Designer.cs
+++ TheApp/TheApp.Droid/Resources/Resource.Designer.cs
@@ -26,7 +26,6 @@ namespace TheApp.Droid
 		
 		public static void UpdateIdValues()
 		{
-			global::Splat.Resource.String.library_name = global::TheApp.Droid.Resource.String.library_name;
 		}
 		
 		public partial class Attribute
@@ -54,22 +53,6 @@ namespace TheApp.Droid
 			}
 			
 			private Drawable()
-			{
-			}
-		}
-		
-		public partial class String
-		{
-			
-			// aapt resource value: 0x7f030000
-			public const int library_name = 2130903040;
-			
-			static String()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private String()
 			{
 			}
 		}
